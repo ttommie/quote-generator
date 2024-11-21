492bf84394192e81eef2e56a70a5403512b17ed9
ttommie <devtommie@gmail.com>
2024-11-20 19:03:21 -0500

    fix(buttons): adjust hover effect on buttons & add github svg

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ function Header() {
       <h1 className="font-inter text-4xl">quote generator</h1>
 
       {/* Color Mode */}
-      <div className="border-2 w-10 h-10 rounded-lg"></div>
+      <div className="border-2 w-10 h-10 rounded-lg hover:shadow-[inset_0px_0px_50px_0px_#ffffff1A]"></div>
     </div>
   )
 }
--- src/components/Quote.tsx
+++ src/components/Quote.tsx
@@ -2,16 +2,20 @@ function Quote() {
   return (
     <div className='items-center w-[474px] space-y-4'>
       {/* Quote */}
-      <p>
+      <p className="font-inter text-[15px] select-text">
         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
         standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make
         a type specimen book.
       </p>
-      
+
       {/* Buttons */}
-      <div className="space-x-[5px]">
-        <button className="border-2 rounded-lg w-[230px] h-10">generate</button>
-        <button className="border-2 rounded-lg w-[230px] h-10 border-[#2F4794]">copy</button>
+      <div className='space-x-[5px]'>
+        <button className='border-2 rounded-lg w-[230px] h-10 hover:shadow-[inset_0px_0px_50px_0px_#FFFFFF1A]'>
+          generate
+        </button>
+        <button className='border-2 rounded-lg w-[230px] h-10 border-[#2F4794] hover:shadow-[inset_0px_0px_50px_0px_#3F75C61A]'>
+          copy
+        </button>
       </div>
     </div>
   );
