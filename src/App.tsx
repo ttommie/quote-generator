7ebe87a2de9e73d549c499a357480a5990235c9e
ttommie <devtommie@gmail.com>
2024-11-20 19:59:44 -0500

    feat(colorIcons): remove comments & import color mode icons

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,13 @@ import Quote from './components/Quote';
 function App() {
   return (
     <div className="flex flex-col min-h-screen">
+      
       <div className='flex flex-col flex-grow justify-center items-center space-y-4'>
-        {/* Header */}
         <Header />
-
-        {/* Quote */}
         <Quote />
       </div>
+
       <div>
-        {/* Footer */}
         <Footer />
       </div>
     </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,15 @@
+import LightMode from '../assets/lightMode.svg'
+import DarkMode from '../assets/darkMode.svg'
+
 const Header = () => (
     <div className="flex justify-between items-center w-[474px]">
       {/* Title */}
       <h1 className="font-inter text-4xl">quote generator</h1>
 
       {/* Color Mode */}
-      <div className="border-2 w-10 h-10 rounded-lg hover:shadow-[inset_0px_0px_50px_0px_#ffffff1A]"></div>
+      <div className=" flex justify-center items-center border-2 w-10 h-10 rounded-lg hover:shadow-[inset_0px_0px_50px_0px_#ffffff1A]">
+        <img src={LightMode} alt="lightmode" width="17" height="25" />
+      </div>
     </div>
   )
 
