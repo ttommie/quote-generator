2aea4f4da90181cdabe382650b1249e1e3a4953a
ttommie <devtommie@gmail.com>
2024-11-20 19:02:39 -0500

    feat(footer): add footer and correct spacing between components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
-import Header from "./components/Header"
-import Quote from "./components/Quote"
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Quote from './components/Quote';
 
 function App() {
   return (
-    <div className="flex flex-col min-h-screen justify-center items-center space-y-4">
-      {/* Header */}
-      <Header />
+    <div className="flex flex-col min-h-screen">
+      <div className='flex flex-col flex-grow justify-center items-center space-y-4'>
+        {/* Header */}
+        <Header />
 
-      {/* Quote */}
-      <Quote />
+        {/* Quote */}
+        <Quote />
+      </div>
+      <div>
+        {/* Footer */}
+        <Footer />
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <div className="flex justify-end">
+      {/* Comment */}
+      <h1 className="font-inter text-[11px] text-white/50 uppercase mr-1 mb-1">crafted with 🤍 tommy 2024</h1>
+    </div>
+  )
+}
+
+export default Footer
