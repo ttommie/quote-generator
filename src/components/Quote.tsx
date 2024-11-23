192186b4d1ef30d3fc10085532985d1be3b93fbd
ttommie <devtommie@gmail.com>
2024-11-22 20:37:38 -0500

    feat(quote): import axios and make an api call to generate random quote

--- src/components/Quote.tsx
+++ src/components/Quote.tsx
@@ -1,24 +1,62 @@
-function Quote() {
+import { useState } from 'react'
+import axios from 'axios'
+
+const Quote = () => {
+  const [quoteText, setQuoteText] = useState("press 'generate' for a quote")
+  const [authorText, setAuthorText] = useState('')
+
+  const CopyToClip = () => {
+    const textElement = document.getElementById('quote')
+    if (!textElement) return // TODO: Create Error Alert
+
+    const range = document.createRange()
+    const selection = window.getSelection()
+    range.selectNodeContents(textElement)
+    selection?.removeAllRanges()
+    selection?.addRange(range)
+
+    navigator.clipboard
+      .writeText(selection?.toString() || '')
+      .then(() => console.log('TEXT COPIED')) //TODO: Create Copied Notification
+      .catch((err) => console.error('failed to copy text: ' + err)) //TODO: Create Error Alert
+  }
+
+  const FetchQuote = () => {
+    axios
+      .get('https://api.quotable.io/random')
+      .then((res) => {
+        setQuoteText(`${res.data.content}`)
+        setAuthorText(`— ${res.data.author}`)
+      })
+      .catch((error) => console.error('Error fetching quote:', error))
+  }
+
   return (
     <div className='items-center w-[474px] space-y-4'>
       {/* Quote */}
-      <p className='font-inter text-[15px] select-text'>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
-        standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make
-        a type specimen book.
+      <p id='quote' className='font-inter text-[15px] select-text'>
+        {quoteText}
+        <br />
+        <i>{authorText}</i>
       </p>
 
       {/* Buttons */}
       <div className='flex justify-between'>
-        <button className='border-2 rounded-lg w-[230px] h-11 border-border hover:bg-buttonColor/20'>
+        <button
+          className='border-2 rounded-lg w-[230px] h-11 border-border hover:bg-buttonColor/20'
+          onClick={FetchQuote}
+        >
           generate
         </button>
-        <button className='border-2 rounded-lg w-[230px] h-11 border-borderAccent hover:bg-buttonAccentColor/20'>
+        <button
+          className='border-2 rounded-lg w-[230px] h-11 border-borderAccent hover:bg-buttonAccentColor/20'
+          onClick={CopyToClip}
+        >
           copy
         </button>
       </div>
     </div>
-  );
+  )
 }
 
-export default Quote;
+export default Quote
